70aac391c55558934ef890f72bf4aac3257cb39a
bsturdyuk <b.sturdyuk@gmail.com>
2018-01-15 20:58:14 +0700

    Nav Menu & Animation

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-   
+
+  $('.menu-toggle').on('click', function(){
+   $('.full-menu').toggleClass('full-menu--open');
+  })
+
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('full-menu--open');
+	});
 
 });
-
-

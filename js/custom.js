6d577607f2b282dcc7fa6949f27cc3cf06f400ac
bsturdyuk <b.sturdyuk@gmail.com>
2018-01-30 11:20:38 +0000

    Responsive fixes -- who we are

--- js/custom.js
+++ js/custom.js
@@ -189,3 +189,29 @@ $(window).load(function(){
   'use strict';
   sliderTeam.init();
 });
+
+
+
+
+//Category filter Feature
+  $(".filter-button").click(function(){
+       var value = $(this).attr('data-filter');
+
+       if(value == "all")
+       {
+           //$('.filter').removeClass('hidden');
+           $('.filter').show('1000');
+       }
+       else
+       {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+           $(".filter").not('.'+value).hide('3000');
+           $('.filter').filter('.'+value).show('3000');
+       }
+   });
+
+   if ($(".filter-button").removeClass("active")) {
+     $(this).removeClass("active");
+   }
+     $(this).addClass("active");

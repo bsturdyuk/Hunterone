efc69ceb20ccd2d3582693c6dcb6cb4783fafddd
bsturdyuk <b.sturdyuk@gmail.com>
2018-02-06 16:40:04 +0000

    Landing style fixes

--- js/client-icons.js
+++ js/client-icons.js
@@ -1,21 +1,21 @@
-// This module handles the logic for detirmining if the client 
-// icons are in the viewport and animating them. 
+// This module handles the logic for detirmining if the client
+// icons are in the viewport and animating them.
 var ClientIcons = (function(){
   var animated = false;
   var slider = $('#slider-1');
 
   return {
-    
+
     animated: function() {
       return animated;
     },
-    
+
     areInViewport: function() {
       var elementTop = slider.offset().top;
       var elementBottom = elementTop + slider.outerHeight();
       var viewportTop = $(window).scrollTop();
       var viewportBottom = viewportTop + $(window).height();
-      
+
       return elementBottom > viewportTop && elementTop < viewportBottom;
     },
 
@@ -28,13 +28,13 @@ var ClientIcons = (function(){
       var radius = 280;
       var center_top = (slider.height() - $("#icon-0").height()) / 2;
       var center_left = (slider.width() - $("#icon-0").width()) / 2;
-      
+
       $(elems).css('opacity', '0').each(function(i) {
         elem = elems[i];
         x = radius * Math.cos(angle) + center_left;
         y = radius * Math.sin(angle) + center_top;
 
-        $(elem).delay(500*i).animate({
+        $(elem).delay(300*i).animate({
           'position': 'absolute',
           'left': x + 'px',
           'top': y + 'px',
